85d827d53b5a15a0217d1152f15b71b23326fa04
Andrew Nathan <nate@camnetit.net>
2025-08-14 13:33:30 +1000

    updates

--- rules/essential8.Rule.ps1
+++ rules/essential8.Rule.ps1
@@ -1,6 +1,6 @@
-Rule 'E8.MFA.AdminsEnabled' -Tag @{ e8 = 'MFA'; maturity = 'ML2' } -Level Error {
+﻿Rule ''E8.MFA.AdminsEnabled'' -Tag @{ e8 = ''MFA''; maturity = ''ML2'' } -Level Error {
     $TargetObject.mfaForAdmins -eq $true
 }
-Rule 'E8.RegularBackups.Enabled' -Tag @{ e8 = 'RegularBackups'; maturity = 'ML2' } -Level Warning {
+Rule ''E8.RegularBackups.Enabled'' -Tag @{ e8 = ''RegularBackups''; maturity = ''ML2'' } -Level Warning {
     $TargetObject.backupsEnabled -eq $true
 }
